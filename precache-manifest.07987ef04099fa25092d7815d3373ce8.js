3fb21d461840b238d48e16b90893546697d0d7d7
Lili <iliyanastefanova@msn.com>
2019-04-27 20:25:25 -0400

    Updates

---
+++ precache-manifest.07987ef04099fa25092d7815d3373ce8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "065ba44fdcf0bf009546565f2d45b994",
+    "url": "/index.html"
+  },
+  {
+    "revision": "191a1425c92b0c1fa911",
+    "url": "/static/css/main.cd12977e.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "191a1425c92b0c1fa911",
+    "url": "/static/js/main.228f19e6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "37a46256771352f4235c61e017f85265",
+    "url": "/static/media/background.37a46256.jpg"
+  }
+]);
